d503a215336bbda75912a0a3dca5f04449487d6f
MARVIN HEGI <MARVIN_HEGI@sluz.ch>
2025-03-28 09:19:07 +0100

    Aufgabe2: Schere Stein Papier test und Code implementation

--- src/rock-paper-scissors.ts
+++ src/rock-paper-scissors.ts
@@ -1,4 +1,24 @@
 export function play(player1: string, player2: string): number {
-  console.log(player1, player2);
-  throw new Error("not implemented yet");
+  player1 = player1.trim().toLowerCase();
+  player2 = player2.trim().toLowerCase();
+
+ 
+  if ((player1 != "rock" && player1 != "paper" && player1 != "scissors") || (player2 != "rock" && player2 != "paper" && player2 != "scissors")) 
+    {
+    throw new Error('Ungültiger Spielzug');
+  }
+ 
+
+  if  (player1 == player2) 
+  {
+    return 0;
+  }
+  else if ((player1 == "rock" && player2 == "scissors") || (player1 == "scissors" && player2 == "paper") || (player1 == "paper" && player2 == "rock")) 
+  {
+    return 1;
+  } 
+  else 
+  {
+        return 2; }
 }
+
